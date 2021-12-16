56bca439a14edd0ab2448cd554df62c6966faadc
Ayse Kotil <ayse.kotil@tum.de>
2021-12-16 23:29:22 +0300

    Rework tests

--- tests.py
+++ tests.py
@@ -1,14 +1,10 @@
 import unittest
-import warnings
 
 import numpy as np
 from qiskit import QuantumCircuit
-from qiskit.circuit.library import SdgGate
 from qiskit.converters import circuit_to_dag, dag_to_circuit
 
 import optimizations
-
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class TestEquivalences(unittest.TestCase):
@@ -31,7 +27,22 @@ class TestEquivalences(unittest.TestCase):
         qc.h(0)
         qc.h(1)
 
-        return qc
+        dag = circuit_to_dag(qc)
+
+        # Reference optimized quantum circuit
+        qc_ref = QuantumCircuit(2)
+        qc_ref.cx(1, 0)
+        qc_ref.rz(np.pi, 0)
+        qc_ref.rz(np.pi, 1)
+        qc_ref.cx(1, 0)
+
+        # Apply the optimization procedure
+        reduction = optimizations.HGateReduction(dag)
+        dag = reduction.apply()
+        qc_optimized = dag_to_circuit(dag)
+
+        # The circuit should not change
+        assert qc_ref == qc_optimized
 
     # S gate transformation
     def test_p_transformation_1(self):
@@ -43,16 +54,47 @@ class TestEquivalences(unittest.TestCase):
         qc.h(0)
         qc.s(0)
         qc.h(0)
-        return qc
+        dag = circuit_to_dag(qc)
+
+        # Reference optimized quantum circuit
+        qc_ref = QuantumCircuit(1)
+        qc_ref.sdg(0)
+        qc_ref.h(0)
+        qc_ref.sdg(0)
+        qc_ref.rz(np.pi, 0)
+        qc_ref.sdg(0)
+        qc_ref.h(0)
+        qc_ref.sdg(0)
+
+        # Apply the optimization procedure
+        reduction = optimizations.HGateReduction(dag)
+        dag = reduction.apply()
+        qc_optimized = dag_to_circuit(dag)
+
+        # The circuit should not change
+        assert qc_ref == qc_optimized
 
     # S dagger gate transformation
     def test_p_transformation_2(self):
         qc = QuantumCircuit(1)
         qc.h(0)
-        sgd = SdgGate()
-        qc.append(sgd, [0])
+        qc.sdg(0)
         qc.h(0)
-        return qc
+        dag = circuit_to_dag(qc)
+
+        # Reference optimized quantum circuit
+        qc_ref = QuantumCircuit(1)
+        qc_ref.s(0)
+        qc_ref.h(0)
+        qc_ref.s(0)
+
+        # Apply the optimization procedure
+        reduction = optimizations.HGateReduction(dag)
+        dag = reduction.apply()
+        qc_optimized = dag_to_circuit(dag)
+
+        # The circuit should not change
+        assert qc_ref == qc_optimized
 
     # H - P - CNOT - P_dagger - H = P_dagger - CNOT - P
     def test_p_transformation_3(self):
@@ -60,21 +102,47 @@ class TestEquivalences(unittest.TestCase):
         qc.h(1)
         qc.s(1)
         qc.cx(0, 1)
-        sdg = SdgGate()
-        qc.append(sdg, [1])
+        qc.sdg(1)
         qc.h(1)
-        return qc
+        dag = circuit_to_dag(qc)
+
+        # Reference optimized quantum circuit
+        qc_ref = QuantumCircuit(2)
+        qc_ref.sdg(1)
+        qc_ref.cx(0, 1)
+        qc_ref.s(1)
+
+        # Apply the optimization procedure
+        reduction = optimizations.HGateReduction(dag)
+        dag = reduction.apply()
+        qc_optimized = dag_to_circuit(dag)
+
+        # The circuit should not change
+        assert qc_ref == qc_optimized
 
     # H - P_dagger - CNOT - P - H = P - CNOT - P_dagger
     def test_p_transformation_4(self):
         qc = QuantumCircuit(2)
         qc.h(1)
-        sdg = SdgGate()
-        qc.append(sdg, [1])
+        qc.sdg(1)
         qc.cx(0, 1)
         qc.s(1)
         qc.h(1)
-        return qc
+        dag = circuit_to_dag(qc)
+
+        # Reference optimized quantum circuit
+        qc_ref = QuantumCircuit(2)
+        qc_ref.s(1)
+        qc_ref.cx(0, 1)
+        qc_ref.sdg(1)
+
+        # Apply the optimization procedure
+        reduction = optimizations.HGateReduction(dag)
+        dag = reduction.apply()
+        qc_optimized = dag_to_circuit(dag)
+
+        # The circuit should not change
+        assert qc_ref == qc_optimized
 
     def test_rz_commutation_1(self):
         qc = QuantumCircuit(3)
@@ -95,18 +163,24 @@ class TestEquivalences(unittest.TestCase):
 
         # Reference optimized quantum circuit
         qc_ref = QuantumCircuit(3)
-        qc_ref.cx(1, 2)
-        qc_ref.cx(0, 1)
+        qc_ref.rz(np.pi, 0)
+        qc_ref.h(0)
+        qc_ref.cnot(2, 0)
+        qc_ref.h(0)
+        qc_ref.x(0)
+        qc_ref.h(0)
+        qc_ref.cnot(2, 0)
+        qc_ref.h(0)
+        qc_ref.rz(2 * np.pi, 0)
+        qc_ref.cnot(0, 2)
+        qc_ref.rz(np.pi, 0)
 
         # Apply the optimization procedure
         reduction = optimizations.RzReduction(dag)
         dag = reduction.apply()
         qc_optimized = dag_to_circuit(dag)
-        print(qc_optimized.draw())
 
         assert qc_ref == qc_optimized
-
-        return qc
 
     def test_rz_commutation_2(self):
         qc = QuantumCircuit(2)
@@ -115,7 +189,22 @@ class TestEquivalences(unittest.TestCase):
         qc.rz(-np.pi, 1)
         qc.cnot(0, 1)
         qc.rz(np.pi, 1)
-        return qc
+        dag = circuit_to_dag(qc)
+
+        # Reference optimized quantum circuit
+        qc_ref = QuantumCircuit(2)
+        qc_ref.cnot(0, 1)
+        qc_ref.rz(-np.pi, 1)
+        qc_ref.cnot(0, 1)
+        qc_ref.rz(2 * np.pi, 1)
+
+        # Apply the optimization procedure
+        reduction = optimizations.RzReduction(dag)
+        dag = reduction.apply()
+        qc_optimized = dag_to_circuit(dag)
+
+        # The circuit should not change
+        assert qc_ref == qc_optimized
 
     def test_cx_commutation_1(self):
         qc = QuantumCircuit(3)
@@ -123,7 +212,6 @@ class TestEquivalences(unittest.TestCase):
         qc.cx(1, 2)
         qc.cx(0, 1)
         qc.cx(0, 2)
-        print(qc.draw())
         dag = circuit_to_dag(qc)
 
         # Reference optimized quantum circuit
@@ -135,7 +223,6 @@ class TestEquivalences(unittest.TestCase):
         reduction = optimizations.CxReduction(dag)
         dag = reduction.apply()
         qc_optimized = dag_to_circuit(dag)
-        print(qc_optimized.draw())
 
         assert qc_ref == qc_optimized
 
@@ -146,14 +233,12 @@ class TestEquivalences(unittest.TestCase):
         qc.cx(0, 1)
         qc.h(2)
         qc.cx(0, 2)
-        print(qc.draw())
         dag = circuit_to_dag(qc)
 
         # Apply the optimization procedure
         reduction = optimizations.CxReduction(dag)
         dag = reduction.apply()
         qc_optimized = dag_to_circuit(dag)
-        print(qc_optimized.draw())
 
         # The circuit should not change
         assert qc == qc_optimized
@@ -167,7 +252,6 @@ class TestEquivalences(unittest.TestCase):
         qc.cx(0, 2)
         qc.cx(0, 1)
         qc.h(0)
-        print(qc.draw())
         dag = circuit_to_dag(qc)
 
         # Reference optimized quantum circuit
@@ -181,8 +265,6 @@ class TestEquivalences(unittest.TestCase):
         # Apply the optimization procedure
         reduction = optimizations.CxReduction(dag)
         dag = reduction.apply()
-        reduction.animate()
         qc_optimized = dag_to_circuit(dag)
-        print(qc_optimized.draw())
 
         assert qc_ref == qc_optimized
